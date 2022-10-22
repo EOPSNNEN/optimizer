1ef0aea192d9bda446a776208fd32b441c5133ec
K1netikS <69646118+K1netikS@users.noreply.github.com>
2022-10-22 11:47:37 -0600

    Add files via upload

---
+++ optimizer/Program.cs
@@ -0,0 +1,261 @@
+﻿using System.Diagnostics;
+
+namespace optimizer
+{
+    public class Optimize
+    {
+
+        public static void Main()
+        {
+            optimize();
+        }
+
+           
+        public static void optimize()
+        {
+            
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Type 1 to optimize system");
+            string input = Console.ReadLine();
+            int selectedOption;
+            if (int.TryParse(input, out selectedOption))
+            {
+                if (selectedOption == 1)
+                {
+                    Console.WriteLine("\nflushing data...");
+                    flushDNS();
+                    Thread.Sleep(100);
+                    temp();
+                    Thread.Sleep(100);
+                    wintemp();
+                    Thread.Sleep(100);
+                    RecycleBin();
+                    Thread.Sleep(100);
+                    prefetch();
+                    Thread.Sleep(100);
+                    downloads();
+                    Thread.Sleep(100);
+                    logs();
+                    Thread.Sleep(100);
+                    LDlogs();
+                    Thread.Sleep(100);
+                    Xamarin();
+                    Thread.Sleep(100);  
+                    Packages();
+                    Thread.Sleep(100);
+                    D3DSCache();
+                    
+                    
+
+                }
+            }
+        }
+
+        static void flushDNS()
+        {
+            Process cmd = new Process();
+            cmd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.CreateNoWindow = false;
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+            cmd.StandardInput.WriteLine("ipconfig/flushDNS");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+        }
+
+        static void temp()
+        {
+            DirectoryInfo temp = new DirectoryInfo(@"C:\Users\Admin\AppData\Local\Temp");
+
+            try
+            {
+                foreach (FileInfo file in temp.EnumerateFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo direct in temp.EnumerateDirectories())
+                {
+                    direct.Delete(true);
+                }
+            }
+            catch { }
+
+        }
+        static void wintemp()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Windows\Temp");
+
+            try
+            {
+                foreach (FileInfo file in dir.EnumerateFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo direct in dir.EnumerateDirectories())
+                {
+                    direct.Delete(true);
+                }
+            }
+            catch { }
+
+        }
+
+        static void RecycleBin()
+        {
+            DirectoryInfo recycle = new DirectoryInfo(@"C:\$Recycle.Bin");
+
+            try
+            {
+                foreach (FileInfo file in recycle.EnumerateFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo direct in recycle.EnumerateDirectories())
+                {
+                    direct.Delete(true);
+                }
+            }
+            catch { }
+        }
+
+        static void prefetch()
+        {
+            DirectoryInfo prefetch = new DirectoryInfo(@"C:\Windows\Prefetch");
+
+            try
+            {
+                foreach (FileInfo file in prefetch.EnumerateFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo direct in prefetch.EnumerateDirectories())
+                {
+                    direct.Delete(true);
+                }
+            }
+            catch { }
+
+        }
+
+        static void downloads()
+        {
+            DirectoryInfo downloads = new DirectoryInfo(@"C:\Users\Admin\Downloads");
+
+            try
+            {
+                foreach (FileInfo file in downloads.EnumerateFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo direct in downloads.EnumerateDirectories())
+                {
+                    direct.Delete(true);
+                }
+            }
+            catch { }
+            
+        }
+
+
+        static void logs()
+        {
+            DirectoryInfo logs = new DirectoryInfo(@"C:\Windows\Logs");
+
+            try
+            {
+                foreach (FileInfo file in logs.EnumerateFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo direct in logs.EnumerateDirectories())
+                {
+                    direct.Delete(true);
+                }
+            }
+            catch { }
+
+        }
+
+        static void LDlogs()
+        {
+            DirectoryInfo LDlogs = new DirectoryInfo(@"E:\LDPlayer9\vms\leidian0\Logs");
+
+            try
+            {
+                foreach (FileInfo file in LDlogs.EnumerateFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo direct in LDlogs.EnumerateDirectories())
+                {
+                    direct.Delete(true);
+                }
+            }
+            catch { }
+
+        }
+
+
+        static void Xamarin()
+        {
+            DirectoryInfo Xamarin = new DirectoryInfo(@"C:\Users\Admin\AppData\Local\Xamarin");
+
+            try
+            {
+                foreach (FileInfo file in Xamarin.EnumerateFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo direct in Xamarin.EnumerateDirectories())
+                {
+                    direct.Delete(true);
+                }
+            }
+            catch { }
+
+        }
+
+        static void D3DSCache()
+        {
+            DirectoryInfo D3DSCache = new DirectoryInfo(@"C:\Users\Admin\AppData\Local\D3DSCache");
+
+            try
+            {
+                foreach (FileInfo file in D3DSCache.EnumerateFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo direct in D3DSCache.EnumerateDirectories())
+                {
+                    direct.Delete(true);
+                }
+            }
+            catch { }
+
+        }
+
+
+        static void Packages()
+        {
+            DirectoryInfo Packages = new DirectoryInfo(@"C:\Users\Admin\AppData\Local\Packages");
+
+            try
+            {
+                foreach (FileInfo file in Packages.EnumerateFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo direct in Packages.EnumerateDirectories())
+                {
+                    direct.Delete(true);
+                }
+            }
+            catch { }
+
+        }
+
+    }
+}
